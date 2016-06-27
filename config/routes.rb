b71c1db4d6a2a6c7cf070e5790e74da1e091cbe2
Kelsey B <kelseybcoding@gmail.com>
2016-06-27 11:28:59 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+get '/' => 'people#index'
+
+namespace :api  do
+	namespace :v1 do
+		get '/people' => 'people#index'
+		post '/people' => 'people#create'
+		get '/people/:id' => 'people#show'
+	end
+end
+
+end
